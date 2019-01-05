174ad62471e5dafa7720783ec65f9307acfce744
Jordan Gold <jordandgold@gmail.com>
2019-01-05 11:07:25 -0700

    add terra

---
+++ public/scss/terra/js/treeMenu.js
@@ -0,0 +1,21 @@
+/**
+ * Terra Design System
+ * treeMenu.js
+ */
+
+(function($, window, document){
+  
+	'use strict';
+
+	$('.ter-tree-menu__list li.is-expanded').next('ul').show();
+
+    $('.ter-tree-menu__list li.is-expandable > a').click(function () {
+    	if ($(this).parent().hasClass('is-expanded')) {
+    		$(this).next('ul').slideToggle(200).parent().toggleClass('is-expanded');
+    	} else {
+    		$(this).parent().siblings('.is-expanded').find('ul').slideToggle(200).parent().toggleClass('is-expanded');
+    		$(this).next('ul').slideToggle(200).parent().toggleClass('is-expanded');
+    	}
+    });     
+
+})(jQuery, window, document);
